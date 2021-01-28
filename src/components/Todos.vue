e23ff5cae2619515b08d0644a3fdd3334954fa65
Francisco Seco <panchoseur@gmail.com>
2021-01-28 13:03:30 -0300

    integracion todoitem

---
+++ src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todosList">
+            <TodoItem v-bind:todo="todo"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem';
+export default {
+    name:'Todos',
+    props: ['todosList'],
+    components:{TodoItem}
+
+}
+
+</script>
+
+<style>
+
+</style>
